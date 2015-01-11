c6c8360bdf66ec5147f0e81e01343a7c68128a0b
Pola93 <mpolka93@gmail.com>
2015-01-11 13:55:53 +0100

    Jak wygrasz Shootera to sie pokoj tarantino otwiera

---
+++ Assets/Scripts/TarantinoDoor.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TarantinoDoor : MonoBehaviour
+{
+    private bool param = false;
+    private bool doorIsOpen = false;
+    public int speedOpen = 100;
+    void Update()
+    {
+        if (ShooterStarter.gameIsEnabled)
+        {
+            if (!doorIsOpen)
+            {
+                transform.FindChild("body").Translate(Vector3.right * 0.04f * speedOpen);
+            }
+        }
+        if (transform.FindChild("body").transform.position.z < -15.55)
+        {
+            doorIsOpen = true;
+        }
+    }
+}
